d24cee280d6db25552f224a0984363383b0b2724
Zsombor Makker <makker.zsombor@gmail.com>
2022-10-04 14:04:15 +0200

    sokszog crated

--- src/hu/petrik/sokszogoop/Main.java
+++ src/hu/petrik/sokszogoop/Main.java
@@ -1,4 +1,7 @@
 package hu.petrik.sokszogoop;
 
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
---
+++ src/hu/petrik/sokszogoop/Sokszog.java
@@ -0,0 +1,5 @@
+package hu.petrik.sokszogoop;
+
+public class Sokszog {
+
+}
