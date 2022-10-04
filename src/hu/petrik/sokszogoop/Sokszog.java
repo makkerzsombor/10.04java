96a2bac7e19d0f20222e38c52d87944d68369ada
Zsombor Makker <makker.zsombor@gmail.com>
2022-10-04 14:15:59 +0200

    teglalap java done

--- src/hu/petrik/sokszogoop/Sokszog.java
+++ src/hu/petrik/sokszogoop/Sokszog.java
@@ -1,5 +1,17 @@
 package hu.petrik.sokszogoop;
 
 public class Sokszog {
+    private double a;
 
+    public Sokszog(double a) {
+        this.a = a;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
 }
